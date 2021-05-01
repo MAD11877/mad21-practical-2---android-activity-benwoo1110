6c2bfc075de35b6720ab348c99e60097c6dd27e5
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-05-01 17:09:13 +0800

    Add androidTest.

--- app/src/androidTest/java/sg/edu/np/mad/madpractical/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/sg/edu/np/mad/madpractical/ExampleInstrumentedTest.java
@@ -1,14 +1,22 @@
 package sg.edu.np.mad.madpractical;
 
 import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +25,30 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("sg.edu.np.mad.madpractical", appContext.getPackageName());
+    }
+
+    @Test
+    public void testFollowToggle() {
+        onView(withId(R.id.buttonFollow))
+                .perform(click())
+                .check((view, noViewFoundException) -> {
+                    int margin = ((ViewGroup.MarginLayoutParams) view.getLayoutParams()).topMargin;
+
+                    String text = ((Button) view).getText().toString();
+                    Assert.assertEquals(text.toLowerCase(), "unfollow");
+                }).perform(click())
+                .check((view, noViewFoundException) -> {
+                    String text = ((Button) view).getText().toString();
+                    Assert.assertEquals(text.toLowerCase(), "follow");
+                });
     }
 }
